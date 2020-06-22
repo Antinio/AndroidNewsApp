1192fa4f6551b3d1208b99b163589259a3e3e7e3
anton <shramko.ag@gmail.com>
2020-06-22 22:52:37 +0300

    Rename MainActivity to NewsActivity. Add fragments classes. Add packages. Add navigation xml

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -0,0 +1,13 @@
+package com.androiddevs.mvvmnewsapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.androiddevs.mvvmnewsapp.R
+
+class NewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
