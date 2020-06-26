e0cee82494162302da1aaa512e6d086b2a5a122f
anton <shramko.ag@gmail.com>
2020-06-26 16:56:12 +0300

    Add searching news

--- app/src/main/java/ag/shramko/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/ag/shramko/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "f50c330606744b47ae9075c44bff2eed"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
